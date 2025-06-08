c3096158c0e460e2cc83d3204058362682202fcd
nadyarudie <nadyarudie@gmail.com>
2025-06-08 15:40:00 +0700

    edited

--- client/src/models/transaction.js
+++ client/src/models/transaction.js
@@ -2,22 +2,11 @@
 class Transaction {
   constructor({ id, description, amount, type, date, category, userId }) {
     this.id = id;
-    this.description = description;
+    this.title = title;
     this.amount = amount;
     this.type = type; // 'income' or 'expense'
-    this.date = date; // Anda mungkin ingin mengonversi ini ke objek Date
-    this.category = category;
+    this.date = date; 
     this.userId = userId;
   }
-
-  // Contoh method yang relevan dengan transaksi
-  isExpense() {
-    return this.type === 'expense';
-  }
-
-  getFormattedAmount() {
-    return `${this.isExpense() ? '-' : ''}Rp${this.amount.toLocaleString('id-ID')}`;
-  }
 }
-
 export default Transaction;
