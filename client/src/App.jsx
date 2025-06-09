78d083cf16d0dfe0b6d65d13bf1c022bfc5dea64
nadyarudie <nadyarudie@gmail.com>
2025-06-09 08:26:34 +0700

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "@/pages/landing";
-import Login from "@/pages/login";
+import Landing from "@/pages/Landing";
+import Login from "@/pages/Login";
 import Register from "@/pages/Register";
-import Dashboard from "@/pages/dashboard";
-import EditProfile from "@/pages/editProfile";
+import Dashboard from "@/pages/Dashboard";
+import EditProfile from "@/pages/EditProfile";
 
 export default function App() {
   return (
--- server/app.js
+++ server/app.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-<<<<<<< HEAD
-const transactionsRoutes = require('./routes/api/transactions');
-const errorHandler = require('./middlewares/errorHandler');
-=======
 const transactions = require('./routes/api/transactions');
 const authRoutes = require('./routes/api/auth'); // ✅ Tambahkan ini
 const errorHandler = require('./middlewares/errorHandler');
 const authMiddleware = require('./middlewares/authMiddleware');
 
 require('dotenv').config();
->>>>>>> f2e40f4 (Initial commit for cash project)
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-app.use('/api/transactions', transactionsRoutes);
-
-app.use(errorHandler);
-
-module.exports = app;
-=======
 // 🔥 Gunakan authRoutes sebelum middleware login
 app.use('/api/auth', authRoutes); // ✅ Tambahkan ini untuk endpoint /api/auth/login dan /register
 
@@ -38,4 +26,3 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 module.exports = app;
->>>>>>> f2e40f4 (Initial commit for cash project)
